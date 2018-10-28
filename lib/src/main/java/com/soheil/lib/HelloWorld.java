9e31c6ad9f09fe7adbcc9f6ac36ed0ec6ba4501e
Soheil Afzali <SoheilAfzali7@gmail.com>
2018-10-28 21:42:11 +0330

    First Project(Hello World)

---
+++ lib/src/main/java/com/soheil/lib/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.soheil.lib;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        print("Hello World");
+    }
+
+
+    static void print(String msg) {
+        System.out.println(msg);
+    }
+}
---
+++ lib/src/main/java/com/soheil/lib/MyClass.java
@@ -0,0 +1,4 @@
+package com.soheil.lib;
+
+public class MyClass {
+}
