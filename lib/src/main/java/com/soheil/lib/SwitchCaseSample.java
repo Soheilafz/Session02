b75543e82ac1ba8a54f6d49ada79fa550573a1f3
Soheil Afzali <SoheilAfzali7@gmail.com>
2018-10-28 22:08:11 +0330

    Switch Case Sample

---
+++ lib/src/main/java/com/soheil/lib/SwitchCaseSample.java
@@ -0,0 +1,29 @@
+package com.soheil.lib;
+
+public class SwitchCaseSample {
+
+    public static void main(String[] args) {
+        int age = 15;
+
+        switch (age) {
+            case 10:
+                print("Child");
+                break;
+            case 15:
+                print("Teenage");
+                break;
+            case 20:
+                print("Young");
+                break;
+            default:
+                print("Null");
+
+        }
+
+
+    }
+    
+    static void print(String msg) {
+        System.out.println(msg);
+    }
+}
